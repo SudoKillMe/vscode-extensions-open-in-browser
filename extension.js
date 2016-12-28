39a4f8817b645a559c7ee014550e9de20edc38bf
techer <techer@mofunsky.com>
2016-12-28 19:52:27 +0800

    smaill fixed

--- extension.js
+++ extension.js
@@ -1,9 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 var open = require('./src/cmd').open;
 var openByMenu = require('./src/cmd').openByMenu;
-// var File = require('./src/file');
+
 
 function activate(context) {
 
@@ -19,7 +17,6 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
