6a0972f8019ba26f7ac2cc3a184a3e3ca44b4940
TecherLang <348262038@qq.com>
2020-01-20 14:25:39 +0800

    modify

--- src/util.ts
+++ src/util.ts
@@ -27,7 +27,6 @@ export const defaultBrowser = (): string => {
 export const open = (path: string, browser: string = '') => {
   // const name = browser ? browser : standardizedBrowserName(defaultBrowser());
   // const name = standardizedBrowserName(browser);
-  // console.log('path: ', path, ' name: ', name);
   opn(path, { app: browser })
     .catch(_ => {
       vscode.window.showErrorMessage(`Open browser failed!! Please check if you have installed the browser ${browser} correctly!`);
